3bf081d2631bdc9ab16d0215a951990c4eb08466
kimbo <harvi502@gmail.com>
2023-11-30 03:29:20 -0500

    update-5

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,10 @@ import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 import "/node_modules/typed.js/dist/typed.umd.js ";
 import bgChanger from './components/bgChanger.js';
 import text from "./components/text-Transform.js";
+import { slider,slider1 } from "./components/slide.js";
+
+slider1()
+slider()
 text()
 bgChanger()
 
--- src/js/components/slide.js
+++ src/js/components/slide.js
@@ -1,12 +1,16 @@
-function slider(){
+export function slider(){
     
 var swiper = new Swiper('.swiper-container', {
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev'
+	  nextEl: '.swiper-button-next1',
+	  prevEl: '.swiper-button-prev1'
 	},
 	slidesPerView: 1,
 	spaceBetween: 10,
+	autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
 	// init: false,
 	pagination: {
 	  el: '.swiper-pagination',
@@ -33,5 +37,46 @@ var swiper = new Swiper('.swiper-container', {
 	  },
 	} 
     });
+	var swiper1 = new Swiper('.swiper-container1', {
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev'
+		},
+		slidesPerView: 1,
+		spaceBetween: 10,
+		
+		// init: false,
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	
+	  
+		breakpoints: {
+		  620: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		  },
+		  680: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		  },
+		  920: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		  },
+		  1240: {
+			slidesPerView: 4,
+			spaceBetween: 50,
+		  },
+		} 
+		});
+	
+
 }
-export default slider
+export function slider1(){
+    
+	
+	}
+
+	
